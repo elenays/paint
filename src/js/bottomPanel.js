5d436e44e61a16fe8557b6b3fc1bc4a24dba92c5
elenays <helenasp@yandex.ru>
2020-09-18 17:13:54 +0300

    noth

--- src/js/bottomPanel.js
+++ src/js/bottomPanel.js
@@ -25,13 +25,7 @@ export class BottomPanel {
     }
 
     build() {
-        const colors = ['#000', '#787878', '#790300', '#757a01', '#007902', '#007778', '#0a0078',
-            '#7b0077', '#767a38', '#003637', '#286ffe', '#083178', '#4c00fe', '#783b00',
-            '#fff', '#bbbbbb', '#ff0e00', '#faff08', '#00ff0b', '#00feff', '#3400fe',
-            '#ff00fe', '#fbff7a', '#00ff7b', '#76feff', '#8270fe', '#ff0677', '#ff7d36'
-        ]
-
-        colors.forEach((color) => {
+        config.colors.forEach((color) => {
             this.colorPanel.appendChild(this.buildColor(color))
         })
     }
